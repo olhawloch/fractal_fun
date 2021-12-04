c404d4d00b323b28631268ab6f32f19a3a89e6b9
Olha Wloch <olha.wloch@gmail.com>
2021-12-04 16:37:21 -0500

    Some cleaning and added requirements file

--- fractal_plotter.py
+++ fractal_plotter.py
@@ -10,33 +10,59 @@ parser.add_argument('--fractal_type', default='tree', choices=['tree', 'snowflak
 
 args = parser.parse_args()
 
-def build_tree(t, branch_length, shorten_by, angle):
-  if branch_length > MINIMUM_BRANCH_LENGTH:
+def draw_tree(t, branch_length, shorten_length, angle, min_branch_length):
+  if branch_length > min_branch_length:
     t.forward(branch_length)
-    new_length = branch_length - shorten_by
+    new_length = branch_length - shorten_length
     t.left(angle)
-    build_tree(t, new_length, shorten_by, angle)
+    draw_tree(t, new_length, shorten_length, angle, min_branch_length)
     t.right(angle * 2)
-    build_tree(t, new_length, shorten_by, angle)
+    draw_tree(t, new_length, shorten_length, angle, min_branch_length)
     t.left(angle)
     t.backward(branch_length)
 
-def koch_curve(t, iterations, length, shortening_factor, angle):
+def draw_koch_curve(koch, iterations, length, shorten_factor, angle):
   if iterations == 0:
-    t.forward(length)
+    koch.forward(length)
   else:
     iterations = iterations - 1
-    length = length / shortening_factor
-    koch_curve(t, iterations, length, shortening_factor, angle)
-    t.left(angle)
-    koch_curve(t, iterations, length, shortening_factor, angle)
-    t.right(angle * 2)
-    koch_curve(t, iterations, length, shortening_factor, angle)
-    t.left(angle)
-    koch_curve(t, iterations, length, shortening_factor, angle)
+    length = length / shorten_factor
+    draw_koch_curve(koch, iterations, length, shorten_factor, angle)
+    koch.left(angle)
+    draw_koch_curve(koch, iterations, length, shorten_factor, angle)
+    koch.right(angle * 2)
+    draw_koch_curve(koch, iterations, length, shorten_factor, angle)
+    koch.left(angle)
+    draw_koch_curve(koch, iterations, length, shorten_factor, angle)
 
-def mandelbrot(c, z):
-   global iterations
+def fern_set(iterations):
+
+  x = [0]
+  y = [0]
+
+  for i in range(0, iterations): 
+  
+    p = randint(1, 100) 
+      
+    if p == 1: 
+        x.append(0) 
+        y.append(0.16*(y[i])) 
+         
+    if p >= 2 and p <= 86: 
+        x.append(0.85*(x[i]) + 0.04*(y[i])) 
+        y.append(-0.04*(x[i]) + 0.85*(y[i])+1.6) 
+      
+    if p >= 87 and p <= 93: 
+        x.append(0.2*(x[i]) - 0.26*(y[i])) 
+        y.append(0.23*(x[i]) + 0.22*(y[i])+1.6) 
+          
+    if p >= 94 and p <= 100: 
+        x.append(-0.15*(x[i]) + 0.28*(y[i])) 
+        y.append(0.26*(x[i]) + 0.24*(y[i])+0.44)
+
+  return x, y
+
+def mandelbrot(c, z, iterations):
    count = 0
    for a in range(iterations):
       z = z**2 + c
@@ -45,61 +71,50 @@ def mandelbrot(c, z):
          break
    return count
 
-def mandelbrot_set(x, y):
+def mandelbrot_set(x, y, iterations):
    m = np.zeros((len(x), len(y)))
    for i in range(len(x)):
       for j in range(len(y)):
          c = complex(x[i], y[j])
          z = complex(0, 0)
-         count = mandelbrot(c, z)
+         count = mandelbrot(c, z, iterations)
          m[i, j] = count    
    return m
 
 if __name__ == "__main__":
   
   if args.fractal_type == 'tree':
-    MINIMUM_BRANCH_LENGTH = 5
     tree = turtle.Turtle()
-    tree.hideturtle()
+    tree.ht()
+    tree.penup()
+    tree.goto(0,-250)
+    tree.pendown()
     tree.setheading(90)
     tree.color('green')
-    build_tree(tree, 50, 5, 30)
+    tree.speed('fastest')
+    draw_tree(tree, 100, 10, 20, 1)
     turtle.mainloop()
 
   elif args.fractal_type == 'snowflake':
-    t = turtle.Turtle()
-    t.hideturtle()
+    koch = turtle.Turtle()
+    koch.ht()
+    koch.penup()
+    koch.goto(-200,100)
+    koch.pendown()
+    koch.speed('fastest')
+    koch.color("#87CEFA")
     for i in range(3):
-      koch_curve(t, 4, 200, 3, 60)
-      t.right(120)
+      draw_koch_curve(koch, 4, 400, 3, 60)
+      koch.right(120)
     turtle.mainloop()
 
   elif args.fractal_type == 'fern':
 
-    x = [0]
-    y = [0]
-
-    for i in range(0, 50000): 
-    
-      p = randint(1, 100) 
-        
-      if p == 1: 
-          x.append(0) 
-          y.append(0.16*(y[i])) 
-           
-      if p >= 2 and p <= 86: 
-          x.append(0.85*(x[i]) + 0.04*(y[i])) 
-          y.append(-0.04*(x[i]) + 0.85*(y[i])+1.6) 
-        
-      if p >= 87 and p <= 93: 
-          x.append(0.2*(x[i]) - 0.26*(y[i])) 
-          y.append(0.23*(x[i]) + 0.22*(y[i])+1.6) 
-            
-      if p >= 94 and p <= 100: 
-          x.append(-0.15*(x[i]) + 0.28*(y[i])) 
-          y.append(0.26*(x[i]) + 0.24*(y[i])+0.44)
-
-    plt.scatter(x, y, s = 0.2, c ='#5dbb63') 
+    iterations = 50000
+    # create fern set
+    x, y = fern_set(iterations)
+    # plot fern set
+    plt.scatter(x, y, s=0.2, c='#228B22') 
     plt.axis("off")
     plt.show()
 
@@ -109,12 +124,12 @@ if __name__ == "__main__":
     rows = 1000
     cols = 1000
     iterations = 150
-    # creating our x and y arrays
+    # creating x and y arrays
     x = np.linspace(-2, 1, rows)
     y = np.linspace(-1, 1, cols)
     # create our mandelbrot set
-    m = mandelbrot_set(x, y) 
-    # plot the set (best colors: binary, hot, bone, magma)
+    m = mandelbrot_set(x, y, iterations) 
+    # best colors: binary, hot, bone, magma
     plt.imshow(m.T, cmap = "magma")
     plt.axis("off")
     plt.show()
